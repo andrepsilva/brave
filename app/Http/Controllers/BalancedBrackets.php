3c0bf8934b0e016365d8f0fce8473a4b6a992e28
204485023 <apereira@cbmerj.rj.gov.br>
2022-04-24 21:20:51 -0300

    Balanced Brackets finished

--- app/Http/Controllers/BalancedBrackets.php
+++ app/Http/Controllers/BalancedBrackets.php
@@ -33,10 +33,101 @@ class BalancedBrackets extends Controller
 	* @return string
 	*/
     public function index(Request $request){
-		
+
 		$inputString = $request->inputString;
-		echo $inputString;
+		$values = $this->checkPost($request->inputString);
+		$valid = false;
+		$comment = "";
+
+		if($values && $this->checkString($values)){
+			
+			if($this->validateValues($values, $inputString)){
+				$valid = true;
+			}	
+
+		}else{
+			$comment = "string is not valid";
+		}
+
+		if($valid){
+			echo json_encode(array("string" => $inputString, "status" => "valid", "comment" => $comment));
+		}else{
+			echo json_encode(array("string" => $inputString, "status" => "not valid", "comment" => $comment));
+		}
+		
 	}
 
+	public function order($param)
+	{	
 
+		$order =  array("3" => "[]", "2" => "{}", "1" => "()");
+		$position = array_search($param,$order);
+		
+		if($position){
+			return $position;
+		}else{
+			false;
+		}
+	}
+
+	public function validateValues($values, $inputString){
+
+		$orderOld = "";
+
+		for($count = 0; $count < (count($values)/2); $count++){
+				
+			$first = $inputString[0];
+			$last = $inputString[strlen($inputString) - 1];
+
+
+			$orderFirst = $this->order($first.$last);
+
+			
+			if(!$orderFirst){		
+				return false;
+				exit();
+			}
+
+			if($orderOld == ""){
+				$orderOld = $orderFirst;
+			}
+			
+			if($orderFirst > $orderOld){	
+				return false;
+				exit();
+			}
+			$inputString = substr($inputString,1, -1);				
+		}
+
+		return true;
+	}
+
+	public function checkString($values = array()){
+
+		$check = array('(',')','{','}','[',']');
+
+		foreach($values as $key => $value){
+			if(!in_array($value, $check)){
+				
+				return false;				
+			}
+		}
+
+		return $values;
+	}
+
+	public function checkPost($string = null){
+
+		$values = mb_str_split($string);
+		
+		if( (count($values) % 2 == 0)){
+
+			return $values;
+
+		}else{
+			return false;
+		}
+
+	}
+	
 }
